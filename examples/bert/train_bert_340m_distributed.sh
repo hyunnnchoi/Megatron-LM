8cdc69505eb160e5248be3853adf931508fa5da3
hyunnnchoi <37583279+hyunnnchoi@users.noreply.github.com>
2024-09-08 15:38:27 +0900

    Update train_bert_340m_distributed.sh

--- examples/bert/train_bert_340m_distributed.sh
+++ examples/bert/train_bert_340m_distributed.sh
@@ -18,7 +18,7 @@ VOCAB_FILE=$3
 DATA_PATH=$4
 
 export NODE_RANK=${NODE_RANK:-0} # Default to 0 if not set
-export MASTER_ADDR=${MASTER_ADDR:-"bert-4b-training-0.bert-4b-training.default.svc.cluster.local"}
+export MASTER_ADDR=163.152.20.207
 export MASTER_PORT=${MASTER_PORT:-"23456"}
 
 DISTRIBUTED_ARGS=(
